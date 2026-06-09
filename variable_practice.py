863bb3224d336437fa7253c506a0f91572ad6e6c
Ashwin <252923213+Ashwinjat690@users.noreply.github.com>
2026-06-09 18:24:07 +0530

    Add files via upload
    my first program

---
+++ variable_practice.py
@@ -0,0 +1,4 @@
+name = input("enter your name :")
+age = input("enter your age :")
+collage = input("enter your collage name :")
+print("hello " + name + " your age is " + age + " and you are from " + collage)
