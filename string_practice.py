dd65a2a9b367db8266950e5a29fb5faaac2288ce
Ashwin <252923213+Ashwinjat690@users.noreply.github.com>
2026-06-09 18:32:16 +0530

    Add files via upload

---
+++ string_practice.py
@@ -0,0 +1,7 @@
+course = "python for me "
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.find("for"))
+print(course.replace("for me ","for everyone"))
+print('python 'in course)
