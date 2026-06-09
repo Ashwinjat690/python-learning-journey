a2de351cee5e6871d6631dc8fa7789edbeb7d265
Ashwin <252923213+Ashwinjat690@users.noreply.github.com>
2026-06-09 18:28:37 +0530

    Add files via upload
    the basic of calculations

---
+++ operator_program.py
@@ -0,0 +1,12 @@
+number1 = float(input("enter the first number : "))
+number2 = float(input("enter the second number : "))
+
+addition = number1 + number2 
+substration = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+print("the addition of the two numbers is ",addition)
+print("the substration of the two numbers is ",substration) 
+print("the multiplication of the two numbers is ",multiplication)
+print("the division of the two numbers is ",division)
